50c1784d3461e7e1a2d81775a8da7864ccfa0db0
jeong_ahn <ahnjj.dev@gmail.com>
2023-10-05 00:24:56 +0900

    til folder myproject added

---
+++ myProject/company/jeongcompany/jeongcompany/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for jeongcompany project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jeongcompany.settings")
+
+application = get_asgi_application()
